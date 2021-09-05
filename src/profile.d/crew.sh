4fe4007e2fa90becd9856356d4efc8fd9c344816
supechicken <supechicken666@gmail.com>
2021-09-05 11:23:44 +0800

    Update crew.sh

--- src/profile.d/crew.sh
+++ src/profile.d/crew.sh
@@ -1,6 +1,6 @@
 function crew () {
   case "${1}" in
-  'install'|'reinstall'|'postinstall'|'update')
+  'install'|'reinstall'|'postinstall'|'upgrade')
     command crew "${@}" && source "${CREW_PREFIX}/etc/profile"
   ;;
   esac
